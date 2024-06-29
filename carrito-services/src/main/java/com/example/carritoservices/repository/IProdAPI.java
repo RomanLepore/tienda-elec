63abf43efb45a11ec6cf1ec2831f20175f53bfe4
Roman Lepore <romanlepore30@gmail.com>
2024-06-29 20:09:35 -0300

    Se soluciona funcionamiento Feign Carrito a la hora del deploy

--- carrito-services/src/main/java/com/example/carritoservices/repository/IProdAPI.java
+++ carrito-services/src/main/java/com/example/carritoservices/repository/IProdAPI.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import java.util.List;
 
 // consumo microservicio producto a traves de API.
-@FeignClient(name = "producto-services", url = "http://localhost:8081/prod")
+@FeignClient(name = "producto-services", url = "http://producto-container:8081/prod")
 public interface IProdAPI {
     @GetMapping("/todos")
     public List<ProductoDTO> getListaProductos();
